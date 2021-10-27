6b345e16d36a20c795a64e08775bb26ed1a3caa5
VannyMenghong <vannyhong99@outlook.com>
2021-10-27 09:17:58 +0700

    Palindrome Number or not

--- task2.js
+++ task2.js
@@ -1,1 +1,20 @@
+function myfunction() {
+    var num = document.getElementById("input").value;
+    if (num > 0) {
+        document.getElementById("output").innerHTML = "Positive (+) Numbber ."
+    }
+    else if (num == "") {
+        document.getElementById("output").innerHTML = "Please input  Numbber ...!."
+    }
+    else if (num == 0) {
+        document.getElementById("output").innerHTML = "Zero Number ."
+    }
+    else if (isNaN(num)) {
+        document.getElementById("output").innerHTML = num + ": is not a Numbber ..!."
+    }
 
+    else {
+        document.getElementById("output").innerHTML = "Negative(-) Numbber ."
+    }
+
+}
