53eb1110f2b1a6da164ee73c132ee53f3939bb3b
VannyMenghong <vannyhong99@outlook.com>
2021-10-28 00:33:11 +0700

    Product & Cart (update)

--- task4.js
+++ task4.js
@@ -1,24 +1,36 @@
-let addItem = document.getElementsByClassName("add-item");
+/*ខ្ញុំប្រើប្រាស់Modal សម្រាប់បង្ហាញនូវ item ដែលបាន​ select:
+   ទី១​ ះ​  ទាយទិន្នន័យពី item (detail,icon)តាម​​ការបង្កើត class-name 1 ហើយយើងប្រើ ​ e.target 
+   ដើម្បីចាប់យក parentElement ហើយខ្ញុំងអាចប្រើ querySelector(i)& (p)​ ដែលតាងដោយ var detail,var icon
+   បន្ទាប់មក​យើងបង្កើត var modal =`បង្កើតដើម្បើបង្ហាយលើmodal <div class=".... add-cart-${i}"> 
+<button class=" ... delete${i}" onclick="deteletItem(${i})" >Delete this item</button> `  
+ output var modal to Class modal(modal-item)
+append(detail,icon) to modal
+    */
 
-for (var i = 0; i < addItem.length; i++) {
-    addItem[i].addEventListener('click', addItemCart);
+var item = document.getElementsByClassName("item");
+
+for (var i = 0; i < item.length; i++) {
+    item[i].addEventListener('click', addItemCart);
 }
 
-function addItemCart(e) {
-    var child = e.target;
-    var product = child.parentElement;
-    var detail = product.querySelector("p");
-    var icon = product.querySelector("i");
+function addItemCart(ev) {
+    var child = ev.target;
+    var item = child.parentElement;
+    var detail = item.querySelector("p");
+    var icon = item.querySelector("i");
+    i = i + 1; //id for item-cart start from 1 , but item start from 0(index)
 
-    i = i + 1
-    modal = `<div class=" my-0 d-flex justify-content-between  text-dark add-cart-${i}" id=''>
+    var modal; //this modal is use to write program and design item that add to modal-item
+    /*modal = `<div class="d-flex  justify-content-between add-cart-${i} ">
+            <button class="btn btn-danger btn-sm deleteId${i}" oclick="deleteItem(${i});">Delete item</button></div> `*/
+    modal = `<div class=" my-2 d-flex justify-content-between  bg-warning text-dark add-cart-${i} p-2" >
             <button class="btn btn-sm btn-danger deleteId${i}" onclick="deleteItem(${i})" id="deleteId-${i}">Delete form cart</button>
         </div>`
-
-    var add = document.getElementsByClassName("model-get-value")[0].innerHTML += modal;
-    document.getElementsByClassName("add-cart-" + i)[0].append(detail, icon);
+    var add = document.getElementsByClassName("modal-item")[0].innerHTML += modal;
+    document.getElementsByClassName("add-cart-" + i)[0].append(detail, icon)
 }
 
-function deleteItem(value) {
-    document.getElementsByClassName("deleteId" + value)[0].parentElement.remove();
+//delete Function
+function deleteItem(id) {
+    document.getElementsByClassName("deleteId" + id)[0].parentElement.remove();
 }
